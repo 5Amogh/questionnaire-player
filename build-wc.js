cab0f4e911dcc1beea8c4695a41a52d90c8aec42
5Amogh <amoghavarsh@navadhiti.com>
2023-10-12 09:39:51 +0530

    build file for web component

---
+++ build-wc.js
@@ -0,0 +1,19 @@
+const fs = require("fs-extra");
+const concat = require("concat");
+
+const build = async () => {
+  const files = [
+    "./dist/questionnaire-webcomponent/runtime.js",
+    "./dist/questionnaire-webcomponent/polyfills.js",
+    "./dist/questionnaire-webcomponent/main.js",
+  ];
+
+  await fs.ensureDir("dist/questionnaire-webcomponent");
+  await fs.ensureDir("web-component");
+  await concat(files, "web-component/questionnaire-player-webcomponent.js");
+  await fs.copy('./dist/questionnaire-webcomponent/styles.css','web-component/styles.css');
+  console.log('Files concatenated successfully');
+  
+};
+
+build();
