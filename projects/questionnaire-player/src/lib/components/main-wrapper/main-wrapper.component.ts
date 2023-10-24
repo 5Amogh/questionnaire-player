96e4296590ac5d4d2a2ccdd4760b69ad9e2a8d37
5Amogh <amoghavarsh@navadhiti.com>
2023-10-24 13:24:25 +0530

    temp chages to be merged in assessment feature branch (after review)

--- projects/questionnaire-player/src/lib/components/main-wrapper/main-wrapper.component.ts
+++ projects/questionnaire-player/src/lib/components/main-wrapper/main-wrapper.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import {
   AssessmentInfo,
   Evidence,
@@ -13,9 +13,10 @@ import { QuestionnaireService } from '../../services/questionnaire.service';
   templateUrl: './main-wrapper.component.html',
   styleUrls: ['./main-wrapper.component.css'],
 })
-export class MainWrapperComponent implements OnInit {
+export class MainWrapperComponent implements OnInit{
   questions: Array<Question>;
-  @Input() assessmentInfo: AssessmentInfo;
+  @Input({required:true}) assessmentInfo;
+  @Input()tempQuestion;
   evidence: Evidence;
   sections: Section[];
   questionnaireForm: FormGroup;
@@ -25,32 +26,40 @@ export class MainWrapperComponent implements OnInit {
     public questionnaireService: QuestionnaireService
   ) {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes);
-    console.log('on change',this.assessmentInfo)
-    if(changes['assessmentInfo'] && changes['assessmentInfo'].previousValue == undefined && changes['assessmentInfo'].currentValue){
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   // console.log(changes);
+  //   // console.log('on change',this.assessmentInfo)
+  //   // if(changes['assessmentInfo'] && changes['assessmentInfo'].previousValue == undefined && changes['assessmentInfo'].currentValue){
+  //   //   this.assessmentInfo = this.questionnaireService.mapSubmissionToAssessment(this.assessmentInfo)
+  //   //   console.log('assessmentInfo inside the library', this.assessmentInfo);
+  //   //   this.evidence = this.assessmentInfo.assessment.evidences[0];
+  //   //   this.evidence.startTime = Date.now();
+  //   //   this.sections = this.evidence.sections;
+  //   //   this.questions = this.sections[0].questions;
+  //   // }
+  // }
+  ngOnInit() {
+    console.log('assessment on init',this.assessmentInfo)
+    console.log('temp questions',this.tempQuestion);
+    if (typeof this.assessmentInfo === 'string') {
+      try {
+        this.assessmentInfo = JSON.parse(this.assessmentInfo);
+        console.log('questions',this.assessmentInfo)
       this.assessmentInfo = this.questionnaireService.mapSubmissionToAssessment(this.assessmentInfo)
       console.log('assessmentInfo inside the library', this.assessmentInfo);
       this.evidence = this.assessmentInfo.assessment.evidences[0];
       this.evidence.startTime = Date.now();
       this.sections = this.evidence.sections;
-      this.questions = this.sections[0].questions;
+      this.questions = this.sections[0].questions
+        if (!Array.isArray(this.questions)) {
+          throw new Error(
+            'Invalid Question Structure, Please configure questions to be an iterable'
+          );
+        }
+      } catch (error) {
+        throw new Error('Invalid Question Structure', error);
+      }
     }
-  }
-  ngOnInit() {
-    // if (typeof this.questions === 'string') {
-    //   try {
-    //     this.questions = JSON.parse(this.questions);
-    //     console.log('questions',this.questions)
-    //     if (!Array.isArray(this.questions)) {
-    //       throw new Error(
-    //         'Invalid Question Structure, Please configure questions to be an iterable'
-    //       );
-    //     }
-    //   } catch (error) {
-    //     throw new Error('Invalid Question Structure', error);
-    //   }
-    // }
     this.questionnaireForm = this.fb.group({});
   }
 
