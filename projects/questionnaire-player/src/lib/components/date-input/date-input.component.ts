0f9611803ee6dacc2e66c5814be6300ea194b868
5Amogh <amoghavarsh@navadhiti.com>
2024-04-04 17:57:11 +0530

    Auto capture issue fix

--- projects/questionnaire-player/src/lib/components/date-input/date-input.component.ts
+++ projects/questionnaire-player/src/lib/components/date-input/date-input.component.ts
@@ -50,21 +50,28 @@ export class DateInputComponent implements OnInit {
 
   onChange(event) {
     if(!event.value || !event.value._isValid) return
-    const selectedDate = new Date(event.value._d);
-    const tempDate = new Date();
-    selectedDate.setHours(tempDate.getHours());
-    selectedDate.setMinutes(tempDate.getMinutes());
-    this.question.value = selectedDate.toISOString();
+    this.question.value = this.dateSetting(event.value._d);
     this.questionnaireForm.controls[this.question._id].patchValue(
       this.question.value 
     )
     this.question.endTime = Date.now();;
   }
 
+  dateSetting(date:any){
+    const selectedDate = new Date(date);
+    const tempDate = new Date();
+    selectedDate.setHours(tempDate.getHours());
+    selectedDate.setMinutes(tempDate.getMinutes());
+    return selectedDate.toISOString();
+  }
+
   autoCapture() {
+    this.question.value = this.dateSetting(Date.now());
     this.questionnaireForm.controls[this.question._id].patchValue(
       new Date(Date.now())
     );
+    this.question.endTime = Date.now();;
+
   }
 
   get isValid(): boolean {
