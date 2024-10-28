c2d2817e4a20507e08ea177a610ff74d39017ffd
Syed Ameen <syedameenabrar786@gmail.com>
2024-10-28 16:15:10 +0530

    aoi congfig update

--- projects/questionnaire-app/src/app/obs/obs.component.ts
+++ projects/questionnaire-app/src/app/obs/obs.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class ObsComponent {
   apiConfig ={
     baseURL:'https://survey-dev.elevate-apis.shikshalokam.org',
-    userAuthToken:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyNzYsIm5hbWUiOiJWaW5vZEZpdmVhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhIiwic2Vzc2lvbl9pZCI6OTM5Miwib3JnYW5pemF0aW9uX2lkIjoxLCJyb2xlcyI6W3siaWQiOjE2LCJ0aXRsZSI6ImhlYWRfbWFzdGVyIiwibGFiZWwiOiJIZWFkIE1hc3RlciIsInVzZXJfdHlwZSI6MCwic3RhdHVzIjoiQUNUSVZFIiwib3JnYW5pemF0aW9uX2lkIjoyNCwidmlzaWJpbGl0eSI6IlBVQkxJQyJ9XX0sImlhdCI6MTczMDA5NTU4OCwiZXhwIjoxNzMwMTgxOTg4fQ.jQC_Fail1vyoB2gQTkGhV94qjEqBxgRBleBXz5L1gsk',
+    userAuthToken:'',
     solutionType:'observation',
     profileData:{
       "state": "66bf7eb960de1616f42cb984",
@@ -18,7 +18,6 @@ export class ObsComponent {
       "role": "district_education_officer",
       "school":"123e4567-e89b-12d3-a456-426614174042"
     },
-
     solutionId:'66e03d1cbe48d96e6842d25d',
     entityType:'school'
   }
